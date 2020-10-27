9cd8901825af164b219b2d406540347a3b67bfa5
sirx16 <atonuslperry@gmail.com>
2020-10-27 11:20:47 -0600

    commit tree ceed4c7a7248c031bbf3e979d28ac60b0927baff

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    last_element = array.pop
+    if array.length == 0
+        last_element
+     elsif array.length == 1
+          array.join << " and " + last_element
+    else
+        array.join(", ") << ", and " + last_element
+    end
 end
